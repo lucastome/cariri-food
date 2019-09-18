96fe398c42feb5b1a9b2b3415b2292ac36714722
Lucas Tomé <flucastome@gmail.com>
2019-09-17 21:49:14 -0300

    Create projet cariri-food-api

---
+++ cariri-food-api/src/main/java/com/caririfood/CaririFoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.caririfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaririFoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaririFoodApiApplication.class, args);
+	}
+
+}
