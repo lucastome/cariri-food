12be52fb3042584733bc40abcad3efa81eb54630
lucastome <flucastome@gmail.com>
2020-04-05 14:52:39 -0300

    task 4.20 - Update response entity KitchenController

--- cariri-food-api/src/main/java/com/caririfood/controller/KitchenController.java
+++ cariri-food-api/src/main/java/com/caririfood/controller/KitchenController.java
@@ -3,6 +3,8 @@ package com.caririfood.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,8 +26,9 @@ public class KitchenController {
 	}
 	
 	@GetMapping(value = "/{id}")
-	public Kitchen findById(@PathVariable Long id) {
-		return this.kitchenService.findById(id);
+	public ResponseEntity<Kitchen> findById(@PathVariable Long id) {
+		Kitchen kitchen = this.kitchenService.findById(id);
+		return kitchen != null ? ResponseEntity.ok(kitchen) : ResponseEntity.notFound().build();
 	}
 	
 }
