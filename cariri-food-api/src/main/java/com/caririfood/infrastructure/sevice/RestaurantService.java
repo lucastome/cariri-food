31dc6807db19b1df6373e8478f6a9445c6711e05
lucastome <flucastome@gmail.com>
2020-06-29 09:57:22 -0300

    task 4.34 - Partial restaurant update

--- cariri-food-api/src/main/java/com/caririfood/infrastructure/sevice/RestaurantService.java
+++ cariri-food-api/src/main/java/com/caririfood/infrastructure/sevice/RestaurantService.java
@@ -4,11 +4,15 @@ import com.caririfood.domain.exception.EntityNotFoundException;
 import com.caririfood.domain.model.Kitchen;
 import com.caririfood.domain.model.Restaurant;
 import com.caririfood.domain.repository.RestaurantRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ReflectionUtils;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 @Service
@@ -46,4 +50,17 @@ public class RestaurantService {
         BeanUtils.copyProperties(newRestaurant, oldRestaurant, "id");
         return this.save(oldRestaurant);
     }
+
+    public void merge (Map<String,Object> originsFields, Restaurant destinationRestaurant) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Restaurant restaurant = objectMapper.convertValue(originsFields, Restaurant.class);
+
+        originsFields.forEach((nameProperty, valueProperty) -> {
+            Field field = ReflectionUtils.findField(Restaurant.class, nameProperty);
+            field.setAccessible(true);
+            Object newValue = ReflectionUtils.getField(field, restaurant);
+            ReflectionUtils.setField(field, destinationRestaurant, newValue);
+        });
+    }
+
 }
